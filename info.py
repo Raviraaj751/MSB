2591e495e63c0c0626ef198b263d0ce8d7ca4b6c
iFilms Botz <92921270+ifilmsbotz@users.noreply.github.com>
2022-05-16 10:53:31 +0530

    Update info.py

--- info.py
+++ info.py
@@ -52,7 +52,7 @@ LONG_IMDB_DESCRIPTION = is_enabled(environ.get("LONG_IMDB_DESCRIPTION", "False")
 SPELL_CHECK_REPLY = is_enabled(environ.get("SPELL_CHECK_REPLY", "True"), True)
 MAX_LIST_ELM = environ.get("MAX_LIST_ELM", None)
 INDEX_REQ_CHANNEL = int(environ.get('INDEX_REQ_CHANNEL', LOG_CHANNEL))
-MELCOW_NEW_USERS = is_enabled((environ.get('MELCOW_NEW_USERS', "False")), False)
+MELCOW_NEW_USERS = is_enabled((environ.get('MELCOW_NEW_USERS', "True")), True)
 LOGGING = str(environ.get("LOGGING", "True"))
 
 LOG_STR = "Current Cusomized Configurations are:-\n"
