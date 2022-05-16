5c1fd4c6725cd5674d4e40e84521e99883120b36
iFilms Botz <92921270+ifilmsbotz@users.noreply.github.com>
2022-05-16 13:31:56 +0530

    Update p_ttishow.py

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -53,10 +53,10 @@ async def save_group(bot, message):
                     try:
                        button = InlineKeyboardMarkup(
                        [[
-                          InlineKeyboardButton("ɢᴏᴏɢʟᴇ ꜱᴇᴀʀᴄʜ", url=f"https://www.google.com/"),
-                          InlineKeyboardButton("ɪᴍᴅʙ ꜱᴇᴀʀᴄʜ", url=f"https://www.imdb.com/")
+                          InlineKeyboardButton("ᴜᴘᴅᴀᴛᴇs", url=f"t.me/Filmokamella"),
+                          InlineKeyboardButton("ʀᴜʟᴇs", url=f"http://t.me/MissRose_bot?start=rules_-1001114885212")
                        ]])
-                       await message.reply_photo(photo="https://telegra.ph/file/490f26ba76ecc9961c47c.jpg", caption=f"<b>Hᴇʏ {u.mention}, Wᴇʟᴄᴏᴍᴇ To {message.chat.title}</b>\n\n<b> Wʀɪᴛᴇ Oɴʟʏ Mᴏᴠɪᴇ & Sᴇʀɪᴇs Nᴀᴍᴇ.</b>\n\n<b>Usᴇʀ Dᴇᴛᴀɪʟs 🥡 :</b>\n<code>{message.from_user.id}</code>\n\n<b>Mᴜsᴛ Rᴇᴀᴅ Rᴜʟᴇs ᴏꜰ ᴛʜɪꜱ ɢʀᴏᴜᴘ 🎯</b>", reply_markup=button)
+                       await message.reply_photo(photo="https://telegra.ph/file/490f26ba76ecc9961c47c.jpg", caption=f"<b>Hᴇʏ {u.mention} Wᴇʟᴄᴏᴍᴇ To {message.chat.title}</b>\n\n<b>Wʀɪᴛᴇ Oɴʟʏ Mᴏᴠɪᴇ & Sᴇʀɪᴇs Nᴀᴍᴇ.</b>\n\n<b>Usᴇʀ Dᴇᴛᴀɪʟs 🥡 :</b>\n<code>{message.from_user.id}</code>\n\n<b>Mᴜsᴛ Rᴇᴀᴅ Rᴜʟᴇs ᴏꜰ ᴛʜɪꜱ ɢʀᴏᴜᴘ 🎯</b>", reply_markup=button)
                     except:
                         pass
       
